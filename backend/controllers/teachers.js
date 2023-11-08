cf64c117e29eac37a9897770fe2fe81d9cc5e4f2
Alessio Chessa <ale.chessa28@gmail.com>
2023-11-08 22:34:09 +0100

    added controller and service to get all teachers

---
+++ backend/controllers/teachers.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const { getTeachers } = require("../service/teachers.service");
+
+module.exports = {
+  getTeachers: async (req, res) => {
+    try {
+      const teachers = await getTeachers();
+      res.status(200).json({ teachers });
+    } catch (err) {
+      console.error("[BACKEND-SERVER] Cannot get teachers", err);
+      res.status(500).json({ error: "Internal server error has occurred" });
+    }
+  },
+};
---
+++ backend/service/teachers.service.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const db = require("./db");
+const Teacher = require("../model/Teacher");
+
+/**
+ * Get all teachers from the database
+ */
+exports.getTeachers = async () => {
+  try {
+    const teachers = await db.query("SELECT * FROM teacher");
+    return teachers.rows.map(
+      (row) =>
+        new Teacher(
+          row.id,
+          row.surname,
+          row.name,
+          row.email,
+          row.cod_group,
+          row.cod_department
+        )
+    );
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
