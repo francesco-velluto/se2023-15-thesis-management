96051d830f0a1925c8656f9ad080e5b1514df87d
Alessio Chessa <ale.chessa28@gmail.com>
2023-11-09 22:58:59 +0100

    setup backend unit tests for insert proposal

--- backend/tests/proposals.unit.test.js
+++ backend/tests/proposals.unit.test.js
@@ -1,12 +1,15 @@
 "use strict";
 
 const proposalsController = require("../controllers/proposals");
+const { getMaxProposalIdNumber, insertProposal } = require('../service/proposals.service'); // Import your database functions
+
+jest.mock('../service/proposals.service');
 
 beforeAll(() => {
     jest.clearAllMocks();
 });
 
-describe("Proposals Unit Tests", () => {
+describe.skip("Get all proposals Unit Tests", () => {
     test("ERROR 401 | Unauthorized", () => {
         // TODO - this is an example, change it if needed
         const req = {};
@@ -58,5 +61,55 @@ describe("Proposals Unit Tests", () => {
                 description: "Description 2"
             }
         ] });
+    });
+});
+
+describe("Get all proposals Unit Tests", () => {
+    test.skip("T1.1 - ERROR 401 | Unauthorized", () => {
+        const req = {};
+        const res = {
+            status: jest.fn(() => res),
+            json: jest.fn()
+        };
+
+        proposalsController.insertProposal(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(res.json).toHaveBeenCalledWith({ error: "Unauthorized" });
+    });
+
+    test.only("T1.2 - SUCCESS | New proposal inserted", async () => {
+        const mockProposalReq = {
+            title: "Test title",
+            supervisor_id: "T002",
+            keywords: ["k1", "k2"],
+            type: "Master",
+            groups: ["Group A", "Group B"],
+            description: "Test description",
+            required_knowledge: "Node.js, PostgreSQL, React.js",
+            notes: "test notes",
+            expiration_date: "2024-06-30",
+            level: "Undergraduate",
+            cds_programmes: ["CD008"] 
+        };
+        
+        const req = {
+            body: mockProposalReq
+        };
+
+        const res = {
+            status: jest.fn(() => res),
+            json: jest.fn()
+        };
+
+        getMaxProposalIdNumber.mockResolvedValue(10);
+
+        const mockProposalRes = { proposal_id: 11, ...mockProposalReq };
+        insertProposal.mockResolvedValue(mockProposalRes);
+
+        await proposalsController.insertProposal(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(201);
+        expect(res.json).toHaveBeenCalledWith({ proposal: mockProposalRes });
     });
 });
