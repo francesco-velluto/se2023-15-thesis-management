45e6291096ed5f38f218c1d623090af64e0c33dd
Edoardo <s317611@studenti.polito.it>
2023-11-10 17:27:10 +0100

    fix frontend newProposalPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import NewProposalPage from "./pages/NewProposalPage";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/newProposalPage" element = {<NewProposalPage />} />
         </Routes>
     );
 }
