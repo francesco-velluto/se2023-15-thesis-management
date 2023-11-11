913d0761a0ff5c588c2241782d674c26527c6f1b
Francesco Velluto <s317549@studenti.polito.it>
2023-11-11 15:18:08 +0100

    fix dobule declaration of routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProposalsPage from "./pages/ProposalsPage";
