21600c4361faa6d72d6bab9701a883722a336bf4
0-Elia-0 <126093891+0-Elia-0@users.noreply.github.com>
2023-11-16 10:10:57 +0100

    Some little changes in responsivenes of ProposalsPage

--- frontend/src/components/ProposalsSearchArea.js
+++ frontend/src/components/ProposalsSearchArea.js
@@ -150,9 +150,11 @@ function FilterElement(props) {
     return (
       <>
         <Col xs lg="3">
-          <Container className='mx-3 my-1 border border-2 border-dark rounded position-relative'>
-            <strong>{userfriendly_field}</strong> : {((props.fltr.value + "").length < 11 ? props.fltr.value : (props.fltr.value.slice(0, 10) + "..."))}
-    
+          <div className='mx-3 my-1 border border-2 border-dark rounded position-relative'>
+            <span className='filterCardContainer'>
+            <strong>{userfriendly_field}</strong> : {props.fltr.value}
+            </span>
+      
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -170,7 +172,7 @@ function FilterElement(props) {
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
               <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
             </svg>
-          </Container>
+          </div>
         </Col>
       </>
     );
