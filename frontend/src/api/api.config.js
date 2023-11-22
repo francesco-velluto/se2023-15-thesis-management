ddcd3ea115877fb4dd6764d0c598d20091ec69df
Francesco Velluto <s317549@studenti.polito.it>
2023-11-22 17:41:01 +0100

    fix env var

--- frontend/src/api/api.config.js
+++ frontend/src/api/api.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_URL: 'http://localhost:' + process.env.REACT_APP_API_PORT + '/api',
+    API_URL: 'http://localhost:' + process.env.REACT_APP_BACKEND_SERVER_PORT + '/api',
     API_REQUEST_HEADERS: {
         "Content-Type": "application/json",
         "Accept": "application/json"
