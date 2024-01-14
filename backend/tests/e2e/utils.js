3a7238aa0f9d3709b076e5f09624455aad6a9fe4
Alessio Chessa <ale.chessa28@gmail.com>
2024-01-15 00:05:17 +0100

    fix: e2e tests failing due to changing class of login button

--- backend/tests/e2e/utils.js
+++ backend/tests/e2e/utils.js
@@ -4,46 +4,44 @@ const { By, until } = require("selenium-webdriver");
 
 const baseURL = `http://localhost:${process.env.FRONTEND_PORT}`;
 
-module.exports = {    
+module.exports = {
     doLogin: async (username, password, driver) => {
         await driver.get(baseURL);
-    
+
         await driver.sleep(1000);
-    
+
         // perform login
         const usernameBox = await driver.wait(until.elementLocated(By.id("username")), 20000);
         usernameBox.clear();
         await driver.sleep(500);
         usernameBox.sendKeys(username);
-    
+
         const passwordBox = await driver.wait(until.elementLocated(By.id("password")), 20000);
         passwordBox.clear();
         await driver.sleep(500);
         passwordBox.sendKeys(password);
 
         await driver.sleep(2000);
-    
-        // click submit button
-        const submitButton = await driver.findElement(By.xpath('//div[@class="cff0ff2c6"]//button[text()="Continue"]'));
-        await submitButton.click();
-    
+
+        await passwordBox.submit();
+
         await driver.sleep(500);
     },
-    
+
     doLogout: async (driver) => {
         await driver.get(baseURL);
         await driver.sleep(1000);
-    
+
         // click on the drop menu
         const logoutDropdown = await driver.findElement(By.id("dropdown-basic"));
         await logoutDropdown.click();
 
         await driver.sleep(1000);
-    
+
         // click on logout
         const logout = await driver.findElement(By.id("logout-id"));
         await logout.click();
-    
+
         await driver.sleep(1000);
     }
 }
