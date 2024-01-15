4d898cc018eabeba4aa9df7d8cc5d2537f8a96c0
Francesco Velluto <s317549@studenti.polito.it>
2024-01-15 18:04:46 +0100

    unit tests student career

--- backend/tests/unit/students.controller.unit.test.js
+++ backend/tests/unit/students.controller.unit.test.js
@@ -1,4 +1,4 @@
-const { getStudentById } = require("../../service/students.service");
+const { getStudentById, getStudentCareer, hasStudentAppliedForTeacher } = require("../../service/students.service");
 const controller = require("../../controllers/students");
 const Student = require("../../model/Student");
 
@@ -61,5 +61,87 @@ describe("UNIT-CONTROLLER: getStudentById", () => {
     expect(mockRes.json).toHaveBeenCalledWith({
       student: new Student("S001"),
     });
+  });
+});
+
+describe("UNIT-CONTROLLER: getStudentCareer", () => {
+  it("ERROR 404 | Student not found", async () => {
+    const mockReq = {
+      user: {id: "T001"},
+      params: {student_id: "S001"}
+    };
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    getStudentById.mockResolvedValue({data: null});
+
+    await controller.getStudentCareer(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(404);
+    expect(mockRes.json).toHaveBeenCalledWith({error: "Student not found"});
+  });
+
+  it("ERROR 403 | Teacher not authorized to see student's career", async () => {
+    const mockReq = {
+      user: {id: "T001"},
+      params: {student_id: "S001"}
+    };
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    getStudentById.mockResolvedValue({data: new Student("S001")});
+    hasStudentAppliedForTeacher.mockResolvedValue(false);
+
+    await controller.getStudentCareer(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(403);
+    expect(mockRes.json).toHaveBeenCalledWith({error: "Teacher not authorized to see student's career"});
+  });
+
+  it("ERROR 500 | An internal error occurred", async () => {
+    const mockReq = {
+      user: {id: "T001"},
+      params: {student_id: "S001"}
+    };
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    getStudentById.mockImplementation(async () => {
+      throw new Error("Some error");
+    });
+
+    await controller.getStudentCareer(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(500);
+  });
+
+  it("SUCCESS 200 | Should return the corresponding student's career", async () => {
+    const mockReq = {
+      user: {id: "T001"},
+      params: {student_id: "S001"}
+    };
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    getStudentById.mockResolvedValue({data: new Student("S001")});
+    hasStudentAppliedForTeacher.mockResolvedValue(true);
+    getStudentCareer.mockResolvedValue({data: "Computer Science"});
+
+    await controller.getStudentCareer(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith({career: "Computer Science"});
   });
 });
--- backend/tests/unit/students.service.unit.test.js
+++ backend/tests/unit/students.service.unit.test.js
@@ -47,3 +47,73 @@ describe("UNIT-SERVICE: getStudentById", () => {
     );
   });
 });
+
+describe("UNIT-SERVICE: getStudentCareer", () => {
+  it("should return the career of a student", async () => {
+    const mockCareer = [
+      {
+        id: "S001",
+        cod_degree: "L-31",
+        enrollment_year: 2018,
+      },
+    ];
+
+    db.query.mockResolvedValueOnce({
+      rows: mockCareer,
+    });
+
+    const res = await studentService.getStudentCareer("S001");
+
+    expect(res).toEqual({
+      data: mockCareer,
+    });
+  });
+
+  it("should throw error if a generic error occurs on the database", async () => {
+    db.query.mockImplementation(async () => {
+      throw new Error("Internal error");
+    });
+
+    expect(studentService.getStudentCareer("S001")).rejects.toThrow(
+      "Internal error"
+    );
+  });
+});
+
+describe("UNIT-SERVICE: hasStudentAppliedForTeacher", () => {
+  it("should return true if student has applied for teacher", async () => {
+    db.query.mockResolvedValueOnce({
+      rowCount: 1
+    });
+
+    const res = await studentService.hasStudentAppliedForTeacher(
+      "S001",
+      "T001"
+    );
+
+    expect(res).toBeTruthy();
+  });
+
+  it("should return false if student has not applied for teacher", async () => {
+    db.query.mockResolvedValueOnce({
+      rowCount: 0
+    });
+
+    const res = await studentService.hasStudentAppliedForTeacher(
+      "S001",
+      "T001"
+    );
+
+    expect(res).toBeFalsy();
+  });
+
+  it("should throw error if a generic error occurs on the database", async () => {
+    db.query.mockImplementation(async () => {
+      throw new Error("Internal error");
+    });
+
+    expect(
+      studentService.hasStudentAppliedForTeacher("S001", "T001")
+    ).rejects.toThrow("Internal error");
+  });
+});
