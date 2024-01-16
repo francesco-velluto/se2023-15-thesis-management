3faec5cf195410c9a325aac1a037a87f465bff40
Francesco Velluto <s317549@studenti.polito.it>
2024-01-16 16:42:12 +0100

    even cooler loading!

--- frontend/src/components/GenericLoading.js
+++ frontend/src/components/GenericLoading.js
@@ -6,14 +6,14 @@ function GenericLoading({text}) {
   const displayText = text ? text : "Loading";
 
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-      <Card className={"text-center align-items-center justify-content-center"}>
-        <Card.Header className={"w-100 generic-loading-header"}>
+    <Container className="d-flex align-items-center justify-content-center" style={{ height: '100vh'}}>
+      <Card className={"text-center align-items-center justify-content-center bg-transparent border-0"} style={{minWidth: '350px'}}>
+        <Card.Header className={"w-100 generic-loading-header rounded-pill"}>
           <img src={Logo} alt="Logo" width="60" height="auto" className="bi bi-mortarboard-fill me-2"/>
           <br/>
           <span id="navbar-title">THESIS MANAGEMENT</span>
         </Card.Header>
-        <Card.Body>
+        <Card.Body className={"bg-light rounded-5 w-100"}>
           <Spinner animation="grow" className={"generic-loading-spinner"}/>
           <Card.Text>
             {displayText}
